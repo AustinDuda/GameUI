31920e53e9e4228988f7b5c9dcc7f166bb424034
LAPTOP-NGA6NJKS\aduda <aduda0821@gmail.com>
2023-04-22 22:49:25 -0400

    removing console logs and hello api

--- src/pages/api/skills.ts
+++ src/pages/api/skills.ts
@@ -5,33 +5,25 @@ type Data = {
   skills: object
 }
 
+let skills = [
+  {name:'woodcutting', xp: 1000},
+  {name:'mining', xp: 0},
+  {name:'fishing', xp: 0},
+]
+
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ 
-    skills: {
-      woodcutting: {xp: 1000},
-      mining: {xp: 0},
-      fishing: {xp: 0},
+  if (req.method === 'POST') {
+    const newSkillsData = req.body;
+
+    if (newSkillsData.length > 0) {
+      skills = JSON.parse(newSkillsData);
     }
-  })
+    res.status(201).json(newSkillsData);
+  } else {
+    res.status(200).json({skills})
+  }
 }
 
-/*
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const requestMethod = req.method;
-  const body = JSON.parse(req.body);
-  switch (requestMethod) {
-    case 'POST':
-      res.status(200).json({ skills: body })
-    default:
-      res.status(200).json({ 
-        skills: [
-          {id:1, title: 'woodcutting', xp: 0},
-          {id:2, title: 'mining',xp: 0},
-          {id:3, title: 'fishing',xp: 0},
-          ]
-      })
-  }
-}*/
