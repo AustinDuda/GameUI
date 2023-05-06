3c732631e5e3edc175a8fe495dcd86a376614360
KRAKEN\aduda <aduda0821@gmail.com>
2023-05-06 02:09:42 -0500

    testing bank check for item

--- src/components/EventsContainer.tsx
+++ src/components/EventsContainer.tsx
@@ -1,12 +1,15 @@
 /* Imports */
 import React from 'react';
 import styled from 'styled-components';
+import useApiGet from '@/hooks/useApiGet';
+import { PLAYERDATATYPES } from '@/configs/enums';
 
 
 /* Setting styles */
 const EventsWrapper = styled.div`
-
     img {
+        width: 3.2;
+        cursor: pointer;
         margin: 3.2rem auto 0 auto;
     }
 `;
@@ -14,9 +17,20 @@ const EventsWrapper = styled.div`
 
 /* Component */
 export const EventsContainer = () => {
+    const { getData } = useApiGet('/api/playerData', PLAYERDATATYPES.bank);
+
+    /* */
+    const openChest = () => {
+        if (!getData) return;
+        const getItemIndexIfExists = Array.getData.findIndex(item => item.name === 'oak log');
+        console.log(getItemIndexIfExists)
+    }
+
     /* Renderer */
     return (
-        <EventsWrapper>
+        <EventsWrapper
+            onClick={() => { openChest() }}
+        >
             <h1>Events</h1>
             <img src='/images/chest-closed.png' />
         </EventsWrapper>
