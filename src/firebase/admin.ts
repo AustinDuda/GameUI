3b6a3585fb16bace16538d5b05e864addf0c5906
KRAKEN\aduda <aduda0821@gmail.com>
2023-05-13 21:45:21 -0500

    fixing admin docs

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,16 +1,17 @@
 import admin from "firebase-admin";
-import type { ServiceAccount } from "firebase-admin";
 
 if (!admin.apps.length) {
   try {
-    if (!process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
+    if (!process.env.NEXT_PUBLIC_FIREBASE_API_KEY) {
       throw new Error("FIREBASE_SERVICE_ACCOUNT_JSON is not set");
     }
     admin.initializeApp({
-      credential: admin.credential.cert(
-        JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON) as ServiceAccount
-      ),
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET_NAME,
+      credential: admin.credential.cert({
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY ? process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+        clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+      }),
+      storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     });
   } catch (error) {
     if (error instanceof Error) {
